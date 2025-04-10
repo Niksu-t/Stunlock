a62fbe7868c61930d98255a012b1855ed267317b
NikoTie <nikojjt@gmail.com>
2025-04-10 13:29:23 +0300

    Added models for care teams

--- BackEnd/src/caregroup/caregroup_model.js
+++ BackEnd/src/caregroup/caregroup_model.js
@@ -1,6 +1,5 @@
 import promisePool from "../utils/database.js";
 
-
 // search for care teams from database
 export const selectCareTeam = async () => {
   console.log("Searching for Care teams");
@@ -33,16 +32,32 @@ export const addCareTeam = async (team) => {
 // Delete team
 export const delCareTeam = async (id) => {
   console.log("Deleting care team with id: ", id);
-  
-  const query = `DELETE FROM CareGroup WHERE group_id=?`
+
+  const query = `DELETE FROM CareGroup WHERE group_id=?`;
   const params = [id];
   try {
-    const rows = promisePool.query(query,params);
+    const rows = promisePool.query(query, params);
     console.log("Team deleted with: ", id);
-    return {message: "Care team deleted"};
-  }catch (err) {
+    return { message: "Care team deleted" };
+  } catch (err) {
     console.log("Error: ", err);
     return 0;
   }
+};
 
+// Update team
+export const updateCareTeam = async (name, id) => {
+  console.log("Updating care team with id of: ", id);
+
+  const query = `UPDATE CareGroup SET name = ?`;
+  const params = [name];
+
+  try {
+    const rows = promisePool.query(query, params);
+    console.log("Team updated");
+    return { message: "Care team updated" };
+  } catch (err) {
+    console.log("Error: ", err);
+    return 0;
+  }
 };
