d4f2c0b554616a6ba010ef85a1fbcb2a8d8f713e
Veleho <nikojjt@gmail.com>
2025-04-23 12:53:17 +0300

    Deleted unnecessary code

--- BackEnd/src/cache/cache-router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { findData, storeToCache } from "./cache-controller.js";
-
-export const cacheRouter = express.Router();
-
-
-cacheRouter.route("/tocache/:id").post(storeToCache);
-
-cacheRouter.route("/cache/:id").get(findData);
