33791f0ff9709b9816f864bf18c9270b92ad02e3
Veleho <nikojjt@gmail.com>
2025-05-06 22:13:25 +0300

    Finished most of api documentation

--- BackEnd/src/caregroup/caregroup_router.js
+++ BackEnd/src/caregroup/caregroup_router.js
@@ -7,7 +7,28 @@ import { getAllTeams } from "./caregroup_controller.js";
 
 
 export const teamRouter = express.Router();
-
+  /**
+ * @api {GET} /careteams Gets all careteams
+ * @apiName GetTeams
+ * @apiGroup Caregroups
+ * @apiPermission all
+ *
+ * @apiSuccess {String} message message with caregroups
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       Care teams: {"Name: example caregoup, ID: x "}
+ *     }
+ *
+ * @apiError 500 server error.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 500 Forbidden
+ *     {
+ *       "message": "internall server error"
+ *     }
+ */
 teamRouter.route("/").get(getAllTeams);
 
 //teamRouter.route("/:id").put()
--- BackEnd/src/entries/entry_router.js
+++ BackEnd/src/entries/entry_router.js
@@ -8,18 +8,88 @@ import {
   postEntry,
   updateEntry,
 } from "./entry_controller.js";
-import { authenticateToken, authorizeEntry } from "../authentication/auth_middleware.js";
+import {
+  authenticateToken,
+  authorizeEntry,
+} from "../authentication/auth_middleware.js";
 import { validationErrorHandler } from "../utils/error.js";
 
 export const entryRouter = express.Router();
 
 entryRouter
   .route("/:id")
+  /**
+* @api {get} /entries/:id Get a entry by ID
+* @apiName GetEntry
+* @apiGroup Entries
+* @apiPermission token
+*
+* @apiHeader {String} Authorization Bearer token.
+*
+* @apiParam {Number} id Entry ID.
+*
+* @apiDescription Get a entry with ID.
+*
+* @apiSuccess {Object} entry Entry info.
+*
+* @apiSuccessExample Success-Response:
+* HTTP/1.1 200 OK
+* {
+*  "entry_id": 52,
+*  "user_id": 21,
+*  "pain_points": {"TMJ": "TRUE"}
+*  "entry_date": 2025-03-09T22:00:00.000Z,
+*  "stress_gauge": 1,
+*  "pain_gauge": 2,
+*  "stiffness_gauge": 3,
+*  "sleep_gauge": 4,
+*  "notes": lorem ipsum,
+*  "create_at": 2025-03-10T16:14:50.000Z
+* }
+*/
 
   .get(authenticateToken, authorizeEntry, getEntry)
-
+/**
+* @api {delete} /entries/:id Delete a entry by ID
+* @apiName DeleteEntry
+* @apiGroup Entries
+* @apiPermission token
+*
+* @apiHeader {String} Authorization Bearer token.
+*
+* @apiParam {Number} id Entry ID.
+*
+* @apiDescription Delete a entry with ID.
+*
+* @apiSuccess {String} message Query response.
+*
+* @apiSuccessExample Success-Response:
+* HTTP/1.1 200 OK
+* {
+*  "message": "Resource successfully removed",
+* }
+*/
   .delete(authenticateToken, authorizeEntry, deleteEntry)
-
+/**
+* @api {put} /entries/:id Modify a entry by ID
+* @apiName PutEntry
+* @apiGroup Entries
+* @apiPermission token
+*
+* @apiHeader {String} Authorization Bearer token.
+*
+* @apiParam {Number} id Entry ID.
+*
+* @apiDescription Modify a entry with ID.
+*
+* @apiSuccess {String} message Query response.
+*
+* @apiSuccessExample Success-Response:
+* HTTP/1.1 200 OK
+* {
+*  "message": "Resource successfully modified",
+* }
+*/
   .put(
     authenticateToken,
     authorizeEntry,
@@ -35,11 +105,87 @@ entryRouter
 
 entryRouter
   .route("/")
-
+/**
+* @api {get} /entries Get all user entries
+* @apiName PutEntry
+* @apiGroup Entries
+* @apiPermission token
+*
+* @apiHeader {String} Authorization Bearer token.
+**
+* @apiDescription Modify a entry with ID.
+*
+* @apiSuccess {Array} entries Array of entries.
+*
+* @apiSuccessExample Success-Response:
+* HTTP/1.1 200 OK
+* {
+*  "entries": [
+{
+*  "entry_id": 52,
+*  "user_id": 21,
+    "pain_points: {"TMJ: TRUE"}"
+*  "entry_date": 2025-03-09T22:00:00.000Z,
+    "stress_gauge: 1",
+    "pain_gauge: 2",
+    "stiffness_gauge: 3",
+    "sleep_gauge: 4",
+    "notes: lorem ipsum",
+*  "create_at": 2025-03-10T16:14:50.000Z
+* },
+{
+*  "entry_id": 53,
+*  "user_id": 21,
+    "pain_points: {"TMJ: TRUE"}"
+*  "entry_date": 2025-04-09T22:00:00.000Z,
+    "stress_gauge: 2",
+    "pain_gauge: 3",
+    "stiffness_gauge: 4",
+    "sleep_gauge: 6",
+    "notes: lorem ipsum",
+*  "create_at": 2025-03-10T16:14:50.000Z
+* }
+*/
+  
   .get(authenticateToken, getEntries)
-
+/**
+* @api {post} /entries Create a entry.
+* @apiName PostEntry
+* @apiGroup Authentication
+* @apiPermission token
+*
+* @apiHeader {String} Authorization Bearer token.
+*
+* @apiDescription Create a entry.
+*
+* @apiBody {json} pain_points
+* @apiBody {Number} stress
+* @apiBody {Number} pain
+* @apiBody {Number} stiffness
+* @apiBody {Number} sleep
+* @apiBody {String} notes
+*
+* @apiParamExample {json} Request-Example:
+*    {
+*      "pain_points": {"TMJ: true"},
+*      "stress": 5
+*      "pain": 6
+*      "stiffness": 10
+*      "sleep": 5,
+*      "notes": "example"
+*    }
+*
+* @apiSuccess {String} message Entry ID.
+*
+* @apiSuccessExample Success-Response:
+*    HTTP/1.1 200 OK
+*    {
+*      "message": 25
+*    }
+*/
   .post(
     authenticateToken,
+    body("pain_points").notEmpty(),
     body("stress").trim().notEmpty().isInt({ min: 0, max: 10 }),
     body("pain").trim().notEmpty().isInt({ min: 0, max: 10 }),
     body("stiffness").trim().notEmpty().isInt({ min: 0, max: 10 }),
